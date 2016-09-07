fdac5c9ae096a737d941c45f9dfff94b6777158f
alkathirikhaled <kk-kk20@hotmail.com>
2016-09-06 21:37:03 -0400

    initializing files

---
+++ desktop/src/com/alkathirigdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alkathirigdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alkathirigdx.game.AlkathiriGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AlkathiriGdxGame(), config);
+	}
+}
