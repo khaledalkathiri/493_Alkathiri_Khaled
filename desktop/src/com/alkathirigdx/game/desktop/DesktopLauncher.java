b54db321c1d3dbe7e886b6d6814f7a6c44d435f2
khaledalkathiri <kk-kk20@hotmail.com>
2016-09-14 21:23:30 -0400

    adding tools

--- desktop/src/com/alkathirigdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/alkathirigdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,40 @@ package com.alkathirigdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+
+
 //import com.alkathirigdx.game.AlkathiriGdxGame;
 import com.alkathirigdx.game.CanyonBunnyMain;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+
+//to import the packer and the settings
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
+
+
+
+public class DesktopLauncher 
+{
+    private static boolean rebuildAtlas = true;
+    private static boolean drawDebugOutline = true;
+    
+    
+	public static void main (String[] arg) 
+	{
+		if (rebuildAtlas) 
+		{
+            Settings settings = new Settings();
+            settings.maxWidth = 1024;
+            settings.maxHeight = 1024;
+            settings.duplicatePadding = false;
+            settings.debug = drawDebugOutline;
+            TexturePacker.process(settings, "assets-raw/images", "../CanyonBunny-core/assets/images",
+  "canyonbunny.pack");
+}
+
+		
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
